d47129bb425cc654eaaa6dbd78cabd29df177abf
ChrisLynch96 <chlynch@tcd.ie>
2017-03-02 16:32:35 +0000

    Quick mock up of description screen done

---
+++ Android/app/src/main/java/com/app/handi/handi/Activitys/JobDescriptionActivity.java
@@ -0,0 +1,24 @@
+package com.app.handi.handi.Activitys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.app.handi.handi.R;
+
+/**
+ * Created by christopherlynch on 02/03/2017.
+ */
+
+public class JobDescriptionActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_description);
+    }
+
+    public void onClick(View v){
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
