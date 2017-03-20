3a0d8fba7c95977522c7a4b5256950944d1e3543
ChrisLynch96 <chlynch@tcd.ie>
2017-03-20 16:22:48 +0000

    Fleshed out the handi profile a bit more.

--- Android/app/src/main/java/com/app/handi/handi/Activitys/HandiEditProfileActivity.java
+++ Android/app/src/main/java/com/app/handi/handi/Activitys/HandiEditProfileActivity.java
@@ -1,9 +1,12 @@
 package com.app.handi.handi.Activitys;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.app.handi.handi.R;
 
@@ -13,12 +16,39 @@ import com.app.handi.handi.R;
 
 public class HandiEditProfileActivity extends AppCompatActivity {
 
+    private ImageView handiProfilePic;
+
     public void onCreate(Bundle savedBundleInstance){
         super.onCreate(savedBundleInstance);
         setContentView(R.layout.activity_handi_edit_profile);
+        handiProfilePic = (ImageView) findViewById(R.id.activity_handi_edit_profile_handi_picture_image);
     }
 
     public void onClick(View view){
-        startActivity(new Intent(this, HandiHomeActivity.class));
+        if (view.getId() == R.id.activity_handi_edit_profile_button_take_photo){
+            dispatchTakePictureIntent();
+        }
+        else
+            startActivity(new Intent(this, HandiHomeActivity.class));
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //If there is a camera registered to the device then launch it's application.
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Setting the image view that represents the profile pic to the picture that was just taken
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            handiProfilePic.setImageBitmap(imageBitmap);
+        }
     }
 }
--- Android/app/src/main/java/com/app/handi/handi/Activitys/JobDescriptionActivity.java
+++ Android/app/src/main/java/com/app/handi/handi/Activitys/JobDescriptionActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import com.app.handi.handi.R;
 
@@ -18,6 +19,7 @@ public class JobDescriptionActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_description);
+
     }
 
     public void onClick(View v){
