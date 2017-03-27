3cebcfe855456adcde8eea3e423545d20c510e5f
ChrisLynch96 <chlynch@tcd.ie>
2017-03-27 19:26:05 +0100

    Added view job description layout for Killian and made the java classes

---
+++ Android/app/src/main/java/com/app/handi/handi/Activitys/AcceptJobDescriptionActivity.java
@@ -0,0 +1,24 @@
+package com.app.handi.handi.Activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.app.handi.handi.R;
+
+/**
+ * Created by christopherlynch on 27/03/2017.
+ */
+
+public class AcceptJobDescriptionActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceBundle){
+        super.onCreate(savedInstanceBundle);
+        setContentView(R.layout.activity_accept_job_details);
+
+    }
+
+    public void onClick(View view) {
+
+    }
+}
---
+++ Android/app/src/main/java/com/app/handi/handi/Activitys/ViewJobDescriptionActivity.java
@@ -0,0 +1,18 @@
+package com.app.handi.handi.Activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.app.handi.handi.R;
+
+/**
+ * Created by christopherlynch on 27/03/2017.
+ */
+
+public class ViewJobDescriptionActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_job_description);
+    }
+}
