cdc1e385ad3fec71eb6687a6e0821fb5712ccb47
ChrisLynch96 <chlynch@tcd.ie>
2017-03-27 19:08:16 +0100

    Updated the font in the choose handi activity to Sorchas font and made a new screen for killain -> activity_accept_job_description

--- Android/app/src/main/java/com/app/handi/handi/Activitys/ChooseHandiTypeActivity.java
+++ Android/app/src/main/java/com/app/handi/handi/Activitys/ChooseHandiTypeActivity.java
@@ -28,18 +28,12 @@ public class ChooseHandiTypeActivity extends AppCompatActivity {
 
         // Loading Font Face
         Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/LT.ttf");
-        // text view label
-        TextView cleaner = (TextView) findViewById(R.id.activity_job_selection_image_text_cleaner);
-        TextView electrician = (TextView) findViewById(R.id.activity_job_selection_image_text_electrician);
-        TextView painter = (TextView) findViewById(R.id.activity_job_selection_image_text_painter);
-        TextView plumber = (TextView) findViewById(R.id.activity_job_selection_image_text_plumber);
-        TextView handiman = (TextView) findViewById(R.id.activity_job_selection_image_text_handiman);
-        // Applying font
-        cleaner.setTypeface(tf);
-        electrician.setTypeface(tf);
-        painter.setTypeface(tf);
-        plumber.setTypeface(tf);
-        handiman.setTypeface(tf);
+        // Loading TextViews into an arrat
+        TextView[] textViews = {(TextView) findViewById(R.id.activity_choose_handi_text_view_header), (TextView) findViewById(R.id.activity_choose_handi_text_view_cleaner),
+                (TextView) findViewById(R.id.activity_choose_handi_text_view_plumber), (TextView) findViewById(R.id.activity_choose_handi_text_view_electrician),
+                (TextView) findViewById(R.id.activity_choose_handi_text_view_handiman), (TextView) findViewById(R.id.activity_choose_handi_text_view_painter)};
+        //setting typeface
+        setTypeFace(textViews, tf);
     }
 
     public void onClick(View view) {
@@ -74,5 +68,11 @@ public class ChooseHandiTypeActivity extends AppCompatActivity {
             intent.putExtra("profession",profession);
             startActivity(intent);
         }
+    }
+
+    //function that changes the an array of TextViews to a certian typeface
+    public void setTypeFace(TextView[] texts, Typeface tf) {
+        for (int i = 0; i < texts.length; i++)
+            texts[i].setTypeface(tf);
     }
 }
